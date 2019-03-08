f3861b670cc08f93c05516676dd03d6ef7d250f2
jafin <jason.finch@gmail.com>
2019-03-09 07:42:41 +1000

    wallabyjs config

---
+++ wallaby.js
@@ -0,0 +1,24 @@
+module.exports = function (wallaby) {
+    return {
+      files: [
+        'chance.js',
+        'test/helpers/*.js'
+      ],
+  
+      tests: [
+        'test/*.js'
+      ],
+      env: {
+        type: 'node',
+        runner: 'node'
+      },
+      compilers: {
+        '**/*.js': wallaby.compilers.babel({
+          // Tell Wallaby to use Ava's Babel preset, necessary if your project doesn't use Babel otherwise.
+          presets: ['@ava/babel-preset-stage-4']
+        })
+      },
+      testFramework: 'ava',
+      debug: true
+    };
+  };
